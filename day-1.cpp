97930912496ac3bcd4c1643eed0700eb1711251d
Ahmet Ala <44473668+alaahmet@users.noreply.github.com>
2022-12-01 06:32:04 +0100

    Add files via upload

---
+++ day-1.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	string s;
+	long ans=0;
+	long sum=0;
+	vector<int> v;
+	for(std::string line; std::getline(std::cin, line);){
+		if(line.size()){
+            long x=stoi(line);
+			sum+=x;
+		}
+        else{
+        	v.push_back(sum);
+        	ans=max(sum,ans);
+			sum=0;
+        }
+	}
+
+	v.push_back(sum);
+	ans=max(sum,ans);
+	sort(v.begin(),v.end());
+	int n=v.size();
+	cout << (v[n-1]) << endl;
+	cout << (v[n-1]+v[n-2]+v[n-3]) << endl;
+
+}
+
+ 

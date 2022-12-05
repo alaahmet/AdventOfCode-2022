f10be4f95fe440d2a308d93d1756f3f202ea36f3
Ahmet Ala <44473668+alaahmet@users.noreply.github.com>
2022-12-05 06:44:22 +0100

    Update day-5.cpp

--- day-5.cpp
+++ day-5.cpp
@@ -21,24 +21,20 @@ int main() {
 	s[8] = "JDNHFTZB";
 	s[9] = "TFBNQLH";
 
-
-
 	for(int i = 0; i < lines ; i++){
 		int x, from, to;
 		cin >> x >> from >> to;
 		//removed "move" "from" "to" from the input file with a text editor
 		string t = s[from].substr(0, x);
-		reverse(t.begin(), t.end()); // comment this line for part-1
+		reverse(t.begin(), t.end()); // comment this line for part-2
 		s[to] = t + s[to];
 		string m = s[from].substr(x);
 		s[from].clear();
 		s[from] = m;
-		// cout <<s[1] << " "<< s[2] << " "<<s[3] << endl;
 	}	
 	for(int i = 1; i <= strings; ++i){
 		cout << s[i][0];
 	}
-
 
 }
  
